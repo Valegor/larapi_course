8732ba616cc7d5bcdd63ac5f30b60d3852f2f268
Valegor <medifit2000@yandex.ru>
2021-09-03 11:52:43 +0300

    first api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('categories', 'App\Http\Controllers\Api\CategoryController@index');
+
+
+//Users/valeriygorbunov/Documents/koropapi.loc/app/Http/Controllers/Api/CategoryController.php
+//Route::get('categories/{category}', 'Api\CategoryController@show');
+//Route::post('categories', 'Api\CategoryController@store');
+//Route::put('categories/{category}', 'Api\CategoryController@update');
+//Route::delete('categories/{category}', 'Api\CategoryController@destroy');
+
+// Route::apiResource('categories', 'Api\CategoryController')
+//     ->middleware('auth:sanctum');
+
+// Route::get('products', 'Api\ProductController@index');
